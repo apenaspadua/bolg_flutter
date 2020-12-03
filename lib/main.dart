1988a29740f4941e81b6275480c57842b9007fd8
Padua <padua.matheus13@gmail.com>
2020-12-03 11:08:20 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:blog_flutter/pages/home.page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Blog do Balta',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/pages/home.page.dart
@@ -0,0 +1,54 @@
+import 'package:blog_flutter/model/post.model.dart';
+import 'package:blog_flutter/repositories/post.repository.dart';
+import 'package:flutter/material.dart';
+
+import 'details.page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var repository = new PostRepository();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Blog do balta"),
+      ),
+      body: FutureBuilder(
+        future: repository.getAll(),
+        builder: (ctx, snp) {
+          if (snp.hasData) {
+            List<Post> posts = snp.data;
+            return ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (ctx, i) {
+                return ListTile(
+                  title: Text(posts[i].title),
+                  subtitle: Text(posts[i].author.name),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailsPage(
+                          tag: posts[i].meta.url,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
